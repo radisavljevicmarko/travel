ec7415a0310e635ffaa7c5a4ef905a80350be0e1
radisavljevicmarko <markoza.care1402@gmail.com>
2023-08-24 15:13:06 +0200

    change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import './app.scss'
+
 import Navbar from './Components/Navbar/Navbar'
 import Home from './Components/Home/Home'
 import Main from './Components/Main/Main'
 import Footer from './Components/Footer/Footer'
+
+import './app.css'
 
 export const App = () => {
   return (
