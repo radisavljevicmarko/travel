2a368e53f508f3a0573d8d3f060971097eb53d3f
radisavljevicmarko <markoza.care1402@gmail.com>
2023-08-24 13:19:08 +0200

    Footer done

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export const App = () => {
       <Navbar />
       <Home />
       <Main />
-      {/* <Footer /> */}
+      <Footer />
     </>
   )
 }
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,144 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
+import video2 from "../../Assets/video2.mp4";
+import { FiSend } from "react-icons/fi";
+import { MdOutlineTravelExplore } from "react-icons/md";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { AiFillYoutube } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaTripadvisor } from "react-icons/fa";
+import { FiChevronRight } from "react-icons/fi";
 
 const Footer = () => {
   return (
-    <div>footer</div>
-  )
-}
+    <section className="footer">
+      <div className="videoDiv">
+        <video src={video2} autoPlay loop muted type="video/mp4"></video>
+      </div>
+      <div className="secContent container">
+        <div className="contactDiv flex">
+          <div className="text">
+            <small>KEEP IN TOUCH</small>
+            <h2>Travel with us</h2>
+          </div>
+          <div className="inputDiv flex">
+            <input type="text" placeholder="Enter your email" />
+            <button className="btn flex" type="submit">
+              <FiSend className="icon" />
+            </button>
+          </div>
+        </div>
 
-export default Footer
+        <div className="footerCard flex">
+          <div className="footerIntro flex">
+            <div className="logoDiv">
+              <a href="#" className="logo flex">
+                <MdOutlineTravelExplore className="icon" /> Travel.
+              </a>
+            </div>
+            <div className="footerParagraph">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+              a libero voluptatum accusamus exercitationem, odio fuga, dolores
+              maxime, inventore debitis neque culpa nostrum magnam autem? Fugiat
+              similique placeat voluptates. Modi.
+            </div>
+            <div className="footerSocials">
+              <AiOutlineTwitter className="icon" />
+              <AiFillYoutube className="icon" />
+              <AiFillInstagram className="icon" />
+              <FaTripadvisor className="icon" />
+            </div>
+          </div>
+          <div className="footerLinks flex">
+            <div className="linkGroup">
+              <span className="groupTitle">OUR AGENCY</span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Services
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Insurance
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Agency
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Tourism
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Payment
+              </li>
+            </div>
+            <div className="linkGroup">
+              <span className="groupTitle">PARTNERS</span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Bookings
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Rentcars
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                HostelWorld
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Trivago
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Tripadvisor
+              </li>
+            </div>
+            <div className="linkGroup">
+              <span className="groupTitle">LAST MINUT</span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                London
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                California
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Indonesia
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Europe
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Oceania
+              </li>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
